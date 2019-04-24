2c517c83875007be2423db103d937a1332ed993d
pavels3579 <pavels357@yandex.ru>
2019-04-24 23:56:44 +0400

    sign in spec update 1

--- spec/features/sign_in_spec.rb
+++ spec/features/sign_in_spec.rb
@@ -11,8 +11,8 @@ feature 'User can sign in', %q{
   background { visit new_user_session_path }
 
   scenario 'Registered user tries to sign in' do
-    fill_in 'Email', with: 'user@test.com'
-    fill_in 'Password', with: '12345678'
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: user.password
     click_on 'Log in'
 
     expect(page).to have_content 'Signed in successfully.'
