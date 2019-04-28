4b4c69457fc78209de08f397b31682c4305a9cf5
pavels3579 <pavels357@yandex.ru>
2019-04-29 00:34:31 +0400

    User can create the question spec and authentication spec

---
+++ spec/features/sign_out_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+feature 'User can sign out', %q{
+  In order to sign out
+  As an Authenticated user
+  I'd like to be able to sign out
+} do
+
+  given(:user) { create(:user) }
+
+  background { visit root_path }
+
+  scenario 'Registered user tries to sign out' do
+    click_on 'Sign out'
+
+    expect(page).to have_content 'Signed out successfully.'
+  end
+
+end
