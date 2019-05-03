ad5d4373e37d60933acac430f9129a940baebd9a
pavels3579 <pavels357@yandex.ru>
2019-05-03 15:43:01 +0400

    update model spec

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-  has_many :questions, dependent: :destroy
-  has_many :answers, dependent: :destroy
+  has_many :questions, foreign_key: "author_id", dependent: :destroy
+  has_many :answers, foreign_key: "author_id", dependent: :destroy
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,4 +6,19 @@ RSpec.describe User, type: :model do
 
   it { should validate_presence_of :email }
   it { should validate_presence_of :password }
+
+  describe 'its_author?' do
+    let(:user) { create(:user) }
+    let(:another_user) { create(:user) }
+
+    let(:question) { create(:question, author: user) }
+
+    it 'returns true if user is author of resource' do
+      expect(user).to be_its_author(question)
+    end
+
+    it 'returns false if user is not author of resource' do
+      expect(another_user).not_to be_its_author(question)
+    end
+  end
 end
