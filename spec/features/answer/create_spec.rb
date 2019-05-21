810809407d1807fc4f18d6d97ea82941d257864e
pavels3579 <pavels357@yandex.ru>
2019-05-21 23:53:58 +0400

    create answer with attached files spec

--- spec/features/answer/create_spec.rb
+++ spec/features/answer/create_spec.rb
@@ -29,6 +29,16 @@ feature 'User can create answer', %q{
 
       expect(page).to have_content "Body can't be blank"
     end
+
+    scenario 'givs an answer with attached file' do
+      fill_in 'Body', with: 'text answer text'
+
+      attach_file 'File', ["#{Rails.root}/spec/rails_helper.rb", "#{Rails.root}/spec/spec_helper.rb"]
+      click_on 'Create answer'
+
+      expect(page).to have_link 'rails_helper.rb'
+      expect(page).to have_link 'spec_helper.rb'
+    end
   end
 
   scenario 'Unauthenticated user tries to give an answer' do
