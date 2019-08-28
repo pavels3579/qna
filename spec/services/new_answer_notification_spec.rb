542e8cd197600cdb1818b9ab39e7571acb527558
pavels3579 <pavels357@yandex.ru>
2019-08-28 22:37:15 +0400

    add spec does not send new answer notification to non subscribed user

--- spec/services/new_answer_notification_spec.rb
+++ spec/services/new_answer_notification_spec.rb
@@ -1,11 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Services::NewAnswerNotification do
-  let(:user) { create(:user) }
-  let(:question) { create(:question, author: user) }
+  let(:users) { create_list(:user, 2) }
+  let(:question) { create(:question, author: users[0]) }
 
   it 'sends new answer notification to subscribed user' do
-    expect(NewAnswerMailer).to receive(:send_notification).with(user, question).and_call_original
+    expect(NewAnswerMailer).to receive(:send_notification).with(question.author, question).and_call_original
     subject.notify(question)
+  end
+
+  it 'does not send new answer notification to non subscribed user' do
+    expect(NewAnswerMailer).to_not receive(:send_notification).with(users[1], question).and_call_original
   end
 end
