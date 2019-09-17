2ad3591424476191c5a45c8401ca50d0078c0460
pavels3579 <pavels357@yandex.ru>
2019-09-17 22:34:53 +0400

    finished deploy capistrano

---
+++ config/deploy.rb
@@ -0,0 +1,20 @@
+# config valid for current version and patch releases of Capistrano
+lock "~> 3.11.1"
+
+set :application, "qna"
+set :repo_url, "git@github.com:pavels3579/qna.git"
+
+
+# Default deploy_to directory is /var/www/my_app_name
+set :deploy_to, "/home/deployer/qna"
+set :deploy_user, 'deployer'
+
+
+# Default value for :linked_files is []
+append :linked_files, "config/database.yml", "config/master.key"
+
+# Default value for linked_dirs is []
+append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system", 'storage'
+
+set :rvm_ruby_version, '2.6.0'
+#set :bundle_bins, %w{gem rake rails sidekiq}
